54e6b0f38f2e1629f95598529b6ca80dae182f64
riina <54872398+riina@users.noreply.github.com>
2021-12-27 23:57:28 -0800

    add sousei no onmyouji thing (with meta)

--- samples/SnO/SnOProcessor.cs
+++ samples/SnO/SnOProcessor.cs
@@ -1,5 +1,7 @@
 using System.Runtime.InteropServices;
+using System.Text;
 using Fp;
+using Fp.Plus.Images;
 
 FsProcessor.Run<RnRProcessor>(args,
     "SnO",
@@ -17,6 +19,50 @@ public class RnRProcessor : DataProcessor
                     WriteContext<uint> wc = NamePathNoExt.CreateImage(w, h, out Data image);
                     Read(28, MemoryMarshal.Cast<uint, byte>(wc.Source), false);
                     yield return image;
+                    break;
+                }
+            case "fig ":
+                {
+                    int px = i4l[20];
+                    Memory<uint> pxBuf = new uint[px];
+                    Read(24, MemoryMarshal.Cast<uint, byte>(pxBuf.Span), false);
+                    int bPos = 24 + px * 4;
+                    int numImages = i4l[bPos];
+                    for (int i = 0; i < numImages; i++)
+                    {
+                        int cPos = bPos + 4 + i * 20;
+                        int bx = i4l[cPos], by = i4l[cPos + 4], w = i4l[cPos + 8], h = i4l[cPos + 12], o = i4l[cPos + 16];
+                        string name = $"{NameNoExt}_I{i:D4}";
+                        yield return new Rgba32Data((NamePathNoExt / name).ToString(), w, h, pxBuf.Slice(o, w * h));
+                        StringBuilder ib = new();
+                        ib.Append("base_x:").Append(bx).AppendLine();
+                        ib.Append("base_y:").Append(by).AppendLine();
+                        ib.Append("width:").Append(w).AppendLine();
+                        ib.Append("height:").Append(h).AppendLine();
+                        yield return new BufferData<byte>((NamePathNoExt / $"{name}.txt").ToString(), Encoding.UTF8.GetBytes(ib.ToString()));
+                    }
+                    int dPos = bPos + 4 + numImages * 20;
+                    int numSequences = i4l[dPos + 4];
+                    int ePos = dPos + 8;
+                    for (int i = 0; i < numSequences; i++)
+                    {
+                        string id = utf8[ePos, 32].String, type = utf8[ePos, 32 * 2].String, num = utf8[ePos, 32 * 3].String;
+                        ePos += 32 * 3;
+                        int numEntries = i4l[ePos];
+                        ePos += 4;
+                        StringBuilder ib = new();
+                        ib.Append("id:").Append(id).AppendLine();
+                        ib.Append("type:").Append(type).AppendLine();
+                        ib.Append("num:").Append(num).AppendLine();
+                        ib.Append("entries:").Append(numEntries).AppendLine();
+                        for (int j = 0; j < numEntries; j++)
+                        {
+                            ib.Append('(').Append(i4l[ePos]).Append(',').Append(i4l[ePos + 4]).Append(')').AppendLine();
+                            ePos += 8;
+                        }
+                        string name = $"{NameNoExt}_A{i:D4}";
+                        yield return new BufferData<byte>((NamePathNoExt / $"{name}.txt").ToString(), Encoding.UTF8.GetBytes(ib.ToString()));
+                    }
                     break;
                 }
             default:
